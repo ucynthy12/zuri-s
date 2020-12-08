55d02018aab2e545e784d08c417c7c0d2fe07365
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-12-08 02:53:22 +0200

    updated the run file

---
+++ manage.py
@@ -0,0 +1,23 @@
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User,Blog,Comment
+from  flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User = User)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
