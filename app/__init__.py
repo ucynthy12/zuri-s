5c415a3b10d05c7521099beddddf12e33f08c45b
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-12-08 02:53:40 +0200

    initialized the application

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+def create_app(config):
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config])
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
